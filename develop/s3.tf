8ffbdc3f098fa86569222ce136e6aae7f8b4aaa0
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-07-10 01:38:09 +0900

    add: 開発環境のデータレイク用のS3バケットを実装

--- develop/s3.tf
+++ develop/s3.tf
@@ -1,4 +1,126 @@
 # ================================================================================
+# Amazon S3 Bucket for Data Lake
+# ================================================================================
+resource "aws_s3_bucket" "data_lake" {
+  bucket   = "${local.project}-${local.env}-s3-data-lake-bucket"
+  provider = aws.osaka
+
+  tags = {
+    Name = "${local.project}-${local.env}-s3-data-lake-bucket"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+  acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.data_lake,
+  ]
+}
+
+resource "aws_s3_bucket_public_access_block" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.bucket
+
+  rule {
+    blocked_encryption_types = [
+      "SSE-C"
+    ]
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  rule {
+    id     = "transition-and-delete-object"
+    status = "Enabled"
+
+    filter {
+      object_size_greater_than = 0
+    }
+
+    transition {
+      days          = local.transition_days
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = local.expire_days
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = local.transition_days
+      storage_class   = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = local.expire_days
+    }
+  }
+
+  depends_on = [
+    aws_s3_bucket_versioning.data_lake,
+  ]
+}
+
+resource "aws_s3_bucket_policy" "data_lake" {
+  bucket = aws_s3_bucket.data_lake.id
+  policy = data.aws_iam_policy_document.data_lake.json
+}
+
+data "aws_iam_policy_document" "data_lake" {
+  statement {
+    sid    = "EnforceSSL"
+    effect = "Deny"
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      aws_s3_bucket.data_lake.arn,
+      "${aws_s3_bucket.data_lake.arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false",
+      ]
+    }
+  }
+}
+
+
+# ================================================================================
 # Amazon S3 Bucket for Source Code Backup (Osaka)
 # ================================================================================
 resource "aws_s3_bucket" "source_backup_osaka" {
